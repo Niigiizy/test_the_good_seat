f5eb12921aa37318749083b32de7fc24b93060ac
Angeon Nicolas <parsekonlepeu@gmail.com>
2022-05-19 20:05:06 +0200

    verif(api key env): verification key api in environnement variable

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthController } from './auth/auth.controller';
@@ -6,7 +7,14 @@ import { AuthModule } from './auth/auth.module';
 import { CourseSearchModule } from './course-search/course-search.module';
 
 @Module({
-  imports: [AuthModule, CourseSearchModule],
+  imports: [
+    AuthModule, 
+    CourseSearchModule, 
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: ".auth.env"
+    })
+  ],
   controllers: [AppController, AuthController],
   providers: [AppService],
 })
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,12 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class AppService {
+  constructor(private configService: ConfigService) {}
   getHello(): string {
-    return 'Hello World!!!!!!!!';
+    const api_key = process.env.API_KEY || this.configService.get<string>("API_KEY")
+    console.log("api key : ", api_key)
+    return 'Hello World!!!!!!!! '.concat(api_key[0],api_key[1],api_key[2]);
   }
 }
